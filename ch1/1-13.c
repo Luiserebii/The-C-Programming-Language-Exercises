b5cbbe9db5943302688fca6726eed5be4679748d
Luiserebii <luis@serebii.io>
2019-11-22 23:55:29 -0500

    Wow, my epic histogram program actually worked, gg???

--- ch1/1-13.c
+++ ch1/1-13.c
@@ -7,6 +7,10 @@
 main() {
 
     int lengthCount[MAX_LENGTH + 1];
+    //Initialize all of array to 0
+    for(int i = 0; i < MAX_LENGTH + 1; ++i) {
+        lengthCount[i] = 0;
+    }
 
     int c;
     int isInWord = FALSE;
@@ -30,5 +34,30 @@ main() {
 
     for(int i = 0; i < MAX_LENGTH + 1; ++i) {
         printf("%d: %d\n", i, lengthCount[i]);
+    }
+
+    //Write histogram
+    //Obtain highest occurence found
+    int highest = 0;
+    for(int i = 0; i < MAX_LENGTH + 1; ++i) {
+        if(lengthCount[i] > highest) highest = lengthCount[i];
+    }
+    //Now, create histogram:
+
+    //Iterate through downwards, from the highest
+    for(int i = highest; i > 0; --i) {
+        //For each row, 
+        //iterate through each array index
+        for(int index = 0; index < MAX_LENGTH + 1; ++index) {
+            //If this column value is less than row we're on
+            if(lengthCount[index] < i) {
+                //Print a blank
+                putchar('-');
+            } else {
+                putchar('x');
+            }
+        }
+        //At the end of the row, place a newline
+        putchar('\n');
     }
 }
