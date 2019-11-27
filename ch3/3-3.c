f283f77a3d233f0adf6967b2f45fb23df6d31cbf
Luiserebii <luis@serebii.io>
2019-11-27 02:47:45 -0500

    Ok, we're getting somewhere, just one last case we need to fix

--- ch3/3-3.c
+++ ch3/3-3.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
+#define TRUE 1
+#define FALSE 0
+
 void expand(char s1[], char s2[]);
+int iswhitespace(char c);
 
 int main() {
 
-    char a[] = "-Hi, I really like a-z, especially a-z0-9 and a-b-c. Not to mentione -a-z!";
+    char a[] = "-Hi, I really like a-z, especially a-z0-9 and a-b-c. Not to mention -a-z!";
     char b[1000];
 
-    printf("a: %s", a);
+    printf("a: %s\n", a);
     expand(a, b);
-    printf("expand: %s", b);
+    printf("expand: %s\n", b);
 
 }
 
@@ -20,7 +24,7 @@ void expand(char s1[], char s2[]) {
     for(int i = 0, j = 0; i < size; ++i, ++j) {
         switch (s1[i]) {
             case '-':
-                if(i == 0 || i == size - 1) {
+                if(i == 0 || i == size - 1 || iswhitespace(s1[i - 1])) {
                     //Write regularly
                     s2[j] = s1[i];
                 } else {
@@ -28,13 +32,20 @@ void expand(char s1[], char s2[]) {
                     int lim = s1[i + 1];
                     //Loop through the char sequence, using the second as the limiter
                     //We can do this because of the way chars are aligned!
-                    for(int ch = s1[i - 1]; ch <= lim; ++ch) {
-                        
+                    for(int ch = s1[i - 1] + 1; ch <= lim; ++ch) {
+                        //Write the next char to come
+                        s2[j++] = ch;
                     }
+                    //Increment i to skip over the limit char
+                    i += 2;
                 }
             default:
                 s2[j] = s1[i];
         }
     }
 
+}
+
+int iswhitespace(char c) {
+    return (c == ' ') ? TRUE : FALSE;
 }
