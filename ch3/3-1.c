bdfd0dd8fd4cdcdfafc4168a2403304496307ff8
Luiserebii <luis@serebii.io>
2019-11-27 01:33:32 -0500

    Further work on surrounding code with timing stuff

--- ch3/3-1.c
+++ ch3/3-1.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include <time.h>
 
+void printtime(char s[], clock_t start, clock_t end);
+int binsearch(int x, int v[], int n);
+int binsearchnew(int x, int v[], int n);
+
 int main() {
     printf("Timing binary search implementations\n");
     clock_t s1, e1, s2, e2;
 
+    //Time the first
     s1 = clock();
     binsearch();
     e1 = clock();
+    printtime("binsearch: ", s1, e1);
 
-    //Print
-
+    //Time the new
     s2 = clock();
     binsearchnew();
     e2 = clock();
+    printtime("binsearchnew: ", s2, e2);
 
+}
+
+void printtime(char s[], clock_t start, clock_t end) {
+    printf("%s %f sec", s, ((double) (end - start)) / CLOCKS_PER_SEC);
 }
 
 int binsearchnew(int x, int v[], int n) {
