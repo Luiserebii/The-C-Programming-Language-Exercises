2a3f696fece719445592981cd656fd2cbce6e924
Luiserebii <luis@serebii.io>
2019-11-27 02:19:51 -0500

    Added unescape, finished 3-2

--- ch3/3-2.c
+++ ch3/3-2.c
@@ -2,15 +2,20 @@
 #include <string.h>
 
 void escape(char s[], char t[]);
+void unescape(char s[], char t[]);
 
 int main() {
 
     char a[] = "Uguu\nOwO\t\t\n";
     char b[1000];
+    char c[1000];
     printf("a: %s\n", a);
     escape(b, a);
     printf("escape: %s\n", b);
+    unescape(c, b);
+    printf("unescape: %s\n", c);
 }
+
 
 
 void escape(char s[], char t[]) {
@@ -33,3 +38,25 @@ void escape(char s[], char t[]) {
         } 
     }
 }
+
+void unescape(char s[], char t[]) {
+    int len = strlen(t);
+    //j is used to track our writing through s
+    for(int i = 0, j = 0; i < len; ++i, ++j) {
+        switch (t[i]) {
+            case '\\':
+                ++i;
+                switch(t[i])
+                    case 'n':
+                        s[j] = '\n';
+                        break;
+                    case 't':
+                        s[j] = '\t';
+                        break;
+                break;
+            default:
+                s[j] = t[i];
+        } 
+    }
+}
+
