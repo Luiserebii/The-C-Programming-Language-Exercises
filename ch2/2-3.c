cd116627828fc44748c99657efdf4143fecc12b2
Luiserebii <luis@serebii.io>
2019-11-26 22:45:14 -0500

    More fleshed out, very closed to finished

--- ch2/2-3.c
+++ ch2/2-3.c
@@ -6,6 +6,7 @@
 #define FALSE 0;
 
 int htoi(char[] s);
+int isHexDigit(char c);
 int prependedHex(char[] s);
 
 int main() {
@@ -15,8 +16,23 @@ int main() {
 }
 
 int htoi(char[] s) {
+    //Obtain starting point, depending on whether is prepended
     int start = prependedHex(s) ? 2 : 0;
+    //Get length of hex
+    int length = 0;
+    for(int i = start; i < strlen(s) && isHexDigit(s[i]); ++i, ++len)
+        ;
 
+    //Finally, let's calculate the value
+    int val = 0;
+    for(int i = start; i < length; ++i) {
+        val += hdtoi(s[i]) * pow(16, length - i - 1);
+    }
+    return val;
+}
+
+int isHexDigit(char c) {
+    return ('0' <= c && c <= '9') || ('A' <= c && c <= 'F') || ('a' <= c && c <= 'f');
 }
 
 //Determines whether hex is prepended by "0x" or "0X" or not
