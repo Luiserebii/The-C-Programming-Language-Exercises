2d58ab11c021cfe3e0560b2599885875e865678c
Luiserebii <luis@serebii.io>
2019-12-05 01:28:02 -0500

    Finishing up macros

--- ch4/chapter-code/7-macros.c
+++ ch4/chapter-code/7-macros.c
@@ -22,3 +22,25 @@
 //  As an invocation example:
 //    paste(name, 1)   --- expands into --->   name1
 #define paste(front, back) front ## back
+
+//Conditional inclusion is possible using preprocessor control statements
+// 
+//  One way is header-related:
+//  
+//    #if !defined(MEME)
+//    #define MEME
+//
+//    #endif
+//
+//  Another sequence might test a macro variable on what to include:
+//
+//    #if SYSTEM == SYSV
+//        #define HDR "sysv.h"
+//    #elif SYSTEM == BSD
+//        #define HDR "bsd.h"
+//    #elif SYSTEM == MSDOS
+//        #define HDR "msdos.h"
+//    #else
+//        #define HDR "default.h"
+//    #endif
+//    #include HDR
