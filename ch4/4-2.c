d63e7940bc2e918907787385bda9f3182b725efe
Luiserebii <luis@serebii.io>
2019-12-05 21:06:27 -0500

    Sketching out extension of atof for 4-2

---
+++ ch4/4-2.c
@@ -0,0 +1,59 @@
+#include <ctype.h>
+#include <math.h>
+
+//atof: convert string s to double
+double atof(char s[]) {
+    double val, power;
+    int i, sign;
+
+    //Skip whitespace
+    for (i = 0; isspace(s[i]); ++i)
+        ;
+    //Grab sign, if it exists
+    sign = (s[i] == '-') ? -1 : 1;
+    //Advance past sign, if it exists
+    if (s[i] == '+' || s[i] == '-') {
+        ++i;
+    }
+    //Grab all digits and eval to val
+    for (val = 0.0; isdigit(s[i]); ++i) {
+        val = 10.0 * val + (s[i] - '0');
+    }
+    //Advance past ., if it exists
+    if (s[i] == '.') {
+        ++i;
+    }
+    //Grab all digits post-., and keep track of power
+    for (power = 1.0; isdigit(s[i]); ++i) {
+        val = 10.0 * val + (s[i] - '0');
+        power *= 10.0;
+    }
+    
+    //Check if we have an exponent
+    if (s[i] == 'e' || s[i] == 'E') {
+        ++i;
+    }
+    //Grab sign, and advance as needed
+    int expsign;
+    if (s[i] == '-') {
+        expsign = -1;
+        ++i;
+    } else {
+        expsign = 1;
+    }
+    //Grab val
+    double expval;
+    for (expval = 0.0; isdigit(s[i]); ++i) {
+        expval = 10.0 * expval + (s[i] - '0');
+    }
+    
+    //Add to power depending on value obtained
+    if (expsign == -1) {
+        power *= pow(10, expval);
+    } else {
+        power /= pow(10, expval);
+    }
+
+    //Return evaluation based on info collected
+    return sign * val / power;
+}
