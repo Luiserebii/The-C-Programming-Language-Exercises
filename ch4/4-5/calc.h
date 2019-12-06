89d88ba16698a6dd25bc2a8b1e2d142ce342dd58
Luiserebii <luis@serebii.io>
2019-12-05 22:28:04 -0500

    Adding in code, and import, for implementing access to sin, exp, and pow, with $, @, and ^

---
+++ ch4/4-5/calc.h
@@ -0,0 +1,17 @@
+#if !defined(CALC_H) 
+#define CALC_H
+
+#define NUMBER '0'
+
+void push(double);
+double pop(void);
+double stacktop(void);
+double stackduptop(void);
+int stackswaptop(void);
+void stackclear(void);
+
+int getop(char []);
+int getch(void);
+void ungetch(int);
+
+#endif
