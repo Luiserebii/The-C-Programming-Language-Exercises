eae944248a21f63d0b1564e9d81fc1e7b6c3f40b
Luiserebii <luis@serebii.io>
2019-12-19 21:50:51 -0500

    Fixed!

--- ch5/5-3.c
+++ ch5/5-3.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-void strcat(char* s, char* t);
+void strcatI(char* s, char* t);
 
 int main() {
     char uguu[100] = "uwu";
     char* OwO = "Hello memes";
     printf("a: %s   b: %s\n", uguu, OwO);
-    strcat(uguu, OwO);
+    strcatI(uguu, OwO);
     printf("a: %s   b: %s\n", uguu, OwO);
 }
 
-void strcat(char* s, char* t) {
+void strcatI(char* s, char* t) {
     //Find end of s
-    while(*s++)
+    while(*++s)
         ;
     //Copy t in
     while(*s++ = *t++)
