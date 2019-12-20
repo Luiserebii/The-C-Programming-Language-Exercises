28548ec79da0420ac17d120d94f7e5f723b34e96
Luiserebii <luis@serebii.io>
2019-12-19 20:32:45 -0500

    Ok, appears to be running!

---
+++ ch5/chapter-code/2-getint/getint.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "getch.h"
+
+int getint(int *pn);
+
+int main() {
+    
+    for(;;) {
+        int OwO;
+        getint(&OwO);
+        printf("Integer read: %d", OwO);
+    }
+
+}
+
+/* getint:  get next integer from input into *pn */
+int getint(int *pn) {
+    int c, sign;
+
+    //Skip whitespace
+    while(isspace(c = getch()))
+        ;
+    //In the case that it isn't a number, ungetch and return
+    if(!isdigit(c) && c != EOF && c != '+' && c != '-') {
+        ungetch(c);
+        return 0;
+    }
+
+    //Set sign if exists
+    sign = (c == '-') ? -1 : 1;
+    //Skip over sign
+    if(c == '+' || c == '-') {
+        c = getch();
+    }
+    //Generate number char by char until not digit
+    for(*pn = 0; isdigit(c); c = getch()) {
+        *pn = 10 * *pn + (c - '0');
+    }
+    *pn *= sign;
+    //Unless it's EOF, just ungetch the non-digit
+    if(c != EOF) {
+        ungetch(c);
+    }
+
+    return c;
+}
