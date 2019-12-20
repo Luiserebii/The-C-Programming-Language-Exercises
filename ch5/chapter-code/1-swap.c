e1a8fc95b6070adbdab6127dd670f335e6f6c5ec
Luiserebii <luis@serebii.io>
2019-12-19 20:19:29 -0500

    Ahhh, we're segfaulting here; is this because no address is being dereferenced? No memory allocated for an int, just for an int*?

--- ch5/chapter-code/1-swap.c
+++ ch5/chapter-code/1-swap.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
 void printInts(int a, int b);
+void swap(int* a, int* b);
 
 int main() {
     
     int a, b;
     a = 10, b = 20;
     printInts(a, b);
-    swap(a, b);
+    swap(&a, &b);
     printInts(a, b);
+}
+
+void swap(int* a, int* b) {
+    int* temp;
+    *temp = *a;
+    *a = *b;
+    *b = *temp;
 }
 
 void printInts(int a, int b) {
