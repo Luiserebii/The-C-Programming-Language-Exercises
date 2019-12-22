3f3f840a044164426e5b738ec28ffbb7975bc262
Luiserebii <luis@serebii.io>
2019-12-21 21:43:47 -0500

    Beginning to sort this code example out

---
+++ ch5/chapter-code/5-sort-char-arrs/3-alloc.c
@@ -0,0 +1,25 @@
+#define ALLOCSIZE 10000
+
+static char allocbuf[ALLOCSIZE]; /* Storage for alloc */
+static char *allocp = allocbuf; /* Next free position */
+
+//Return pointer to n characters
+char* alloc(int n) {
+
+    //Check if it fits
+    if(allocbuf + ALLOCSIZE - allocp >= n) {
+        allocp += n;
+        return allocp - n; //return pointer
+    } else {
+        //Since 0 is never a valid address type, we can
+        //use this to signal that something is up
+        return 0;
+    }
+}
+
+//Free storage pponted to by p
+void afree(char* p) {
+    if(p >= allocbuf && p < allocbuf + ALLOCSIZE) {
+        allocp = p
+    }
+}
---
+++ ch5/chapter-code/5-sort-char-arrs/lines.h
@@ -0,0 +1,9 @@
+#ifndef LINES_H
+#define LINES_H
+
+#define MAXLEN 1000
+
+int readlines(char* lineptr[], int nlines);
+void writeline(char* lineptr[], int nlines);
+
+#endif
