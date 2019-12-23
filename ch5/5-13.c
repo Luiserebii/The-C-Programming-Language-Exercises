a1f1ae7d7875c8602cb1313f4d6cf34b56832e5d
Luiserebii <luis@serebii.io>
2019-12-22 21:17:24 -0500

    Ok, basic interface piece to 5-13.c done; now to actually write the function

--- ch5/5-13.c
+++ ch5/5-13.c
@@ -8,8 +8,21 @@ void tail(int lines);
 int main(int argc, char* argv[]) {
     
     if(--argc > 1 && (*++argv)[0] == '-') {
+        if((*argv)[1] != 'n') {
+            printf("Invalid option, usage: tail -n num_lines\n");
+            return 1;
+        }
         //Grab number of memes
-
+        int lines = atoi(*++argv);
+        tail(lines);
+        return 0;
     }
 
+    //Call tail with the default number of lines
+    tail(LINES_DEFAULT);
+
+}
+
+void tail(int lines) {
+    printf("%d\n", lines);
 }
