fb2b20c02d25d15b3b32883e18f3e8e481052b0e
Luiserebii <luis@serebii.io>
2019-12-22 21:09:10 -0500

    Beginning work on 5-13

---
+++ ch5/5-13.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LINES_DEFAULT 10
+
+void tail(int lines);
+
+int main(int argc, char* argv[]) {
+    
+    if(--argc > 1 && (*++argv)[0] == '-') {
+        //Grab number of memes
+
+    }
+
+}
