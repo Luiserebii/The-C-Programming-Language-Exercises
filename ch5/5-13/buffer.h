9ef10c171b2d4fd4db34ee89285a5db7c7045a38
Luiserebii <luis@serebii.io>
2019-12-23 19:49:57 -0500

    Setting up a kind of buffer to hold lines into

---
+++ ch5/5-13/buffer.h
@@ -0,0 +1,8 @@
+#ifndef BUFFER_H
+#define BUFFER_H
+
+#define MAX_LINES 50
+extern char* lines[MAX_LINES];
+
+
+#endif
---
+++ ch5/5-13/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "buffer.h"
+
+#define MAX_LEN 1000
+#define LINES_DEFAULT 10
+
+void tail(int lines);
+int getLine(char s[], int lim);
+
+int main(int argc, char* argv[]) {
+    
+    if(--argc > 1 && (*++argv)[0] == '-') {
+        if((*argv)[1] != 'n') {
+            printf("Invalid option, usage: tail -n num_lines\n");
+            return 1;
+        }
+        //Grab number of memes
+        int lines = atoi(*++argv);
+        tail(lines);
+        return 0;
+    }
+
+    //Call tail with the default number of lines
+    tail(LINES_DEFAULT);
+
+}
+
+void tail(int lines) {
+    char line[MAX_LEN];
+    while(getLine(line, MAX_LEN)) {
+        printf("LINE: %s | %d\n", line, lines);
+    }
+
+}
+
+int getLine(char s[], int lim) {
+    int c, i;
+
+    i = 0;
+    while (--lim > 0 && (c=getchar()) != EOF && c != '\n') {
+        s[i++] = c;
+    }
+    if (c == '\n') {
+        s[i++] = c;
+    }
+    s[i] = '\0';
+    return i;
+}
+
