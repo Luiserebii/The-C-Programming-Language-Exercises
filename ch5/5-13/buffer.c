ed2a7d53d5573e6f0e03ac1a863ce773b5797b3d
Luiserebii <luis@serebii.io>
2019-12-23 19:59:07 -0500

    Constructed buffer_push, beginning to sink in now on how to design this

--- ch5/5-13/buffer.c
+++ ch5/5-13/buffer.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
+#include <string.h>
+#include"buffer.h"
 
 char* lines[MAX_LINES];
+
+//Pointer to current piece of buffer
+static char* buffer_curr = lines;
+static char* buffer_end = lines[MAX_LINES] + 1;
+
+void buffer_push(char* line) {
+    if(buffer_curr == buffer_end) {
+        //Loop over and bring back to beginning
+        buffer_curr = lines;
+    }
+
+    //Copy in, and advance buffer_curr
+    strcpy(buffer_curr++, line);
+}
+
