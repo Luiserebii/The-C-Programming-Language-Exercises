88d587097f450ce5ad582daa6191b4e4431703a4
Luiserebii <luis@serebii.io>
2020-01-10 00:16:06 -0500

    Attempting toLowerStr function (been a while, not certain if this will work)

--- ch5/5-15/main.c
+++ ch5/5-15/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "lines.h"
 #include "sort.h"
 
@@ -9,12 +10,14 @@ char* lineptr[MAXLINES];
 
 //void sort(void* lineptr[], int left, int right, int (*comp)(void*, void*));
 //int numcmp(char*, char*);
+void toLowerStr(char* s);
 
 /* Sort input lines */
 int main(int argc, char* argv[]) {
     int nlines;
     int numeric = 0;
     int reverse = 0;
+    int fold = 0;
 
     //Process all memes
     while(--argc){
@@ -22,10 +25,21 @@ int main(int argc, char* argv[]) {
             numeric = 1;  
         } else if(str_cmp(argv[argc], "-r") == 0) {
             reverse = 1;
+        } else if(str_cmp(argv[argc], "-f") == 0) {
+            fold = 1;
         }
     }
 
     if((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+        //This is arguably the least intrusive solution; pre-fold
+        //the strings before comparing, instead of re-implementing each
+        //cmp function to account for treating uppercase/lowercase similarly
+        if(fold) {
+            for(int i = 0; i < nlines; ++i) {
+                toLowerStr(lineptr[i]);
+            }
+        }
+
         q_sort((void**) lineptr, 0, nlines - 1, 
                 (int (*)(void*, void*))(numeric ? numcmp : str_cmp));
         //This is a lazy solution; reverse after the sort has happened
@@ -42,3 +56,9 @@ int main(int argc, char* argv[]) {
     }
 }
 
+void toLowerStr(char* s) {
+    while(*s) {
+        *s = tolower(*s);
+        ++s;
+    }
+}
