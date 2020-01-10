2b46f3dd5c1b91fac7f46a5e6ffbb2b85e4ee1bc
Luiserebii <luis@serebii.io>
2020-01-10 00:04:45 -0500

    Appears to work on one write, kind of surprised. Tackling 5-15 next

---
+++ ch5/5-15/lines.h
@@ -0,0 +1,10 @@
+#ifndef LINES_H
+#define LINES_H
+
+#define MAXLEN 10000
+
+int readlines(char* lineptr[], int nlines);
+void writelines(char* lineptr[], int nlines);
+int getLine(char s[], int lim);
+
+#endif
---
+++ ch5/5-15/sort.h
@@ -0,0 +1,9 @@
+#ifndef SORT_H
+#define SORT_H
+
+void q_sort(void* v[], int left, int right, int (*comp)(void*, void*));
+void swap(void* v[], int i, int j);
+int str_cmp(char* s, char* t);
+int numcmp(char* s1, char* s2);
+
+#endif
