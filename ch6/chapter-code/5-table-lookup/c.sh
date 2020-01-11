1f489b96913ce03cf9de2306b155e8dd6e476a7d
Luiserebii <luis@serebii.io>
2020-01-11 18:33:04 -0500

    Finishing up code insert, now to try using, I suppose

---
+++ ch6/chapter-code/5-table-lookup/c.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cc *.c && ./a.out
--- ch6/chapter-code/5-table-lookup/main.c
+++ ch6/chapter-code/5-table-lookup/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 struct nlist {
     struct nlist* next;
@@ -30,4 +31,27 @@ struct nlist* lookup(char* s) {
     }
     //If we've reached here, nothing found
     return NULL;
+}
+
+/* install:   put (name, defn) in hashtab */
+struct nlist* install(char* name, char* defn) {
+    struct nlist* np;
+    unsigned hashval;
+
+    if((np = lookup(name)) == NULL) {
+        np = (struct nlist*) malloc(sizeof(*np));
+        if(np == NULL || (np->name = strdup(name)) == NULL) {
+            return NULL;
+        }
+        hashval = hash(name);
+        np->next = hashtab[hashval];
+        hashtab[hashval] = np;
+    } else {
+        free((void*) np->defn);
+    }
+
+    if((np->defn = strdup(defn)) == NULL) {
+        return NULL;
+    }
+    return np;
 }
