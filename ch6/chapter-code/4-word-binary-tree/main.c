b9dff90ae152182502d6dfcf46a91ee8b8fe0a93
Luiserebii <luis@serebii.io>
2020-01-11 14:59:08 -0500

    Great, working! Just had to do a quick debug, small typo was there, now to refactor better

--- ch6/chapter-code/4-word-binary-tree/main.c
+++ ch6/chapter-code/4-word-binary-tree/main.c
@@ -26,7 +26,7 @@ int main() {
 
     root = NULL;
     while(getword(word, MAXWORD) != EOF) {
-        if(!isalpha(word[0])) {
+        if(isalpha(word[0])) {
             root = addtree(root, word);
         }
     }
