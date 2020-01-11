a7e304f62e73945c0052f478c378a33991ae5384
Luiserebii <luis@serebii.io>
2020-01-11 14:40:57 -0500

    Fleshing things out a bit more

--- ch6/chapter-code/4-word-binary-tree/main.c
+++ ch6/chapter-code/4-word-binary-tree/main.c
@@ -3,8 +3,20 @@
 #include <string.h>
 
 #define MAXWORD 100
+
+struct tnode {
+    char* word;
+    int count;
+    struct tnode* left;
+    struct tnode* right;
+}
+
 struct tnode* addtree(struct tnode* p, char* w);
 void treeprint(struct tnode* p);
+
+struct tnode* talloc();
+char* strdup(char* s);
+
 int getword(char*, int);
 
 /* Word Frequency count using binary tree */
@@ -21,4 +33,16 @@ int main() {
     }
     treeprint(root);
     return 0; 
+}
+
+/* addtree:   add a node with w, at or below p */
+struct tnode* addnode(struct tnode* p, char* w) {
+    int cond;
+    //If null, then new word has arrived to set
+    if(p == NULL) {
+        p = talloc(); //Allocate a new node
+        p->word = strdup(w); //Duplicate the word, set newly allocated string pointer
+        p->count = 1;
+        p->left = p->right = NULL;
+    }
 }
