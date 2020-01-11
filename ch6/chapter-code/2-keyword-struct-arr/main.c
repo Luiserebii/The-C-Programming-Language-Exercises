95dc6f16a645111061b42080aa7c4a1ff1b58be6
Luiserebii <luis@serebii.io>
2020-01-10 20:38:05 -0500

    Wrote large chunk in, time to add in dependencies/implicit code not in the sample

--- ch6/chapter-code/2-keyword-struct-arr/main.c
+++ ch6/chapter-code/2-keyword-struct-arr/main.c
@@ -29,4 +29,50 @@ int main() {
 
 }
 
+// binsearch:   find word in tab[0]...tab[n-1]
+int binsearch(char* word, struct key tab[], int n) {
+    int cond;
+    int low, high, mid;
 
+    low = 0;
+    high = n - 1;
+    while(low <= high) {
+        mid = (low + high) / 2;
+        if((cond = strcmp(word, tab[mid].word)) < 0) {
+            high = mid - 1;
+        } else if (cond > 0) {
+            low = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    //We didn't find anything, so return -1
+    return -1;
+}
+
+// getword:   get next word or character from input
+int getword(char* word, int lim) {
+    int c;
+    char* w = word;
+    
+    while(isspace(c = getch()))
+        ;
+
+    if(c != EOF) {
+        *w++ = c;
+    }
+
+    if(!isalpha(c)) {
+        *w = '\0';
+        return c;
+    }
+
+    for(; --lim > 0; ++w) {
+        if(!isalnum(*w = getch())) {
+            ungetch(*w);
+            break;
+        }
+    }
+    *w = '\0';
+    return word[0];
+}
