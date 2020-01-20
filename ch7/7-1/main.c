c2144f2db1126340e72c874ced362ed74a36bdd1
Luiserebii <luis@serebii.io>
2020-01-20 03:28:48 -0500

    Adding trimFilename function to account for argv[0] resolving to things like ./meme

--- ch7/7-1/main.c
+++ ch7/7-1/main.c
@@ -4,9 +4,13 @@
 
 void upper();
 void lower();
+void trimFilename(char* s);
 
 int main(int argc, char* argv[]) {
-    
+   
+    //Trim name for comparison, eliminate / in filepath
+    trimFilename(argv[0]);
+
     if(strcmp("upper", argv[0]) == 0) {
         upper();
     } else if(strcmp("lower", argv[0]) == 0) {
@@ -29,4 +33,24 @@ void lower() {
     while((c = getchar()) != EOF) {
         putchar(tolower(c));
     }
+}
+
+/** 
+ * Trims a filename like "./hello" and transforms it into "hello"
+ */
+void trimFilename(char* s) {
+    char* slash;
+    //Iterate through, keep a pointer to the last slash
+    for(char* it = s; *it; ++it) {
+        if(*it == '/') {
+            slash = it;
+        }
+    }
+    
+    //Finally, copy [slash + 1, end) into the string
+    for(++slash; *slash; ++slash) {
+        *s++ = *slash++;
+    }
+    //End with null char
+    *s = '\0';
 }
