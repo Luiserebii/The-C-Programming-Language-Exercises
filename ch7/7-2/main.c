b7890e7e47a57da265381e2a780c3138ae9f8569
Luiserebii <luis@serebii.io>
2020-01-20 20:50:14 -0500

    Ahhh, interesting, appears our refactor is working! I want to break this up into a seperate file now

--- ch7/7-2/main.c
+++ ch7/7-2/main.c
@@ -4,49 +4,71 @@
 #define MAXLINE_CHARS 10
 
 int numDigits(int n, int base);
-
 /* Defining as macro for efficiency */
 #define numOctalDigits(n) numDigits(n, 8)
 
 enum printCharMode { NORMAL, OCTAL };
+int numprint;
+
+int printChar(int c, enum printCharMode mode);
 
 int main() {
 
     int c;
-    int numprint = 0;
+    numprint = 0;
     while((c = getchar()) != EOF) {
         //Apply logic on whether graphic or not using iscntrl
         //Non-graphic chars are control characters
         if(iscntrl(c)) {
-            //Print octal
-            int digits = numOctalDigits(c);
-            if(numprint + digits > MAXLINE_CHARS) {
-                putchar('\n');
-                numprint = 0;
-            }
-            printf("%o", c);
-            numprint += digits;
+            printChar(c, OCTAL);
         } else {
-            //Print char normally
-            if(numprint + 1 > MAXLINE_CHARS) {
-                putchar('\n');
-                numprint = 0;
-            }
-            putchar(c);
-            ++numprint;
+            printChar(c, NORMAL);
         }
     }
 }
 
-void printChar(int c, enum printCharMode mode) {
+/** 
+ * Returns number of chars printed, 0 if failure
+ *
+ * NOTE: Relies on var to maintain global state (numprint);
+ * appropriate as a seperate module
+ */
+int printChar(int c, enum printCharMode mode) {
+    //Determine number of digits to print based on mode
     int digits;
     switch(mode) {
         case NORMAL:
             digits = 1;
+            break;
         case OCTAL:
             digits = numOctalDigits(c);
+            break;
+        default:
+            //We shouldn't reach here; return 0
+            return 0;
+            break;
     }
 
+    //Print newline depending on numchar printed so far
+    if(numprint + digits > MAXLINE_CHARS) {
+        putchar('\n');
+        numprint = 0;
+    }
+
+    //Print the char
+    switch(mode) {
+        case NORMAL:
+            putchar(c);
+            break;
+        case OCTAL:
+            printf("%o", c);
+            break;
+    }
+
+    //Finally, increment numprint by digits
+    numprint += digits;
+
+    return digits;
 }
 
 int numDigits(int n, int base) {
