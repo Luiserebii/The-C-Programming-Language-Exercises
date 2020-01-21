6cfb585b79e3b7484a835b1a70a4482f4a2e4b8b
Luiserebii <luis@serebii.io>
2020-01-20 22:50:51 -0500

    Attempting to build skeleton to test with for 7-4

--- ch7/7-4.c
+++ ch7/7-4.c
@@ -1,17 +1,104 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
+
+#define MAXFORMAT 50
 
 void minscanf(char* fmt, ...);
+int isOption(int c);
 
 int main() {
-    minscanf("OwO", 10, 20);
+    int day, year;
+    char monthname[20];
+
+    int amt = minscanf("%d %s %d", &day, monthname, %year);
+    if(amt == 3) {
+        printf("Successfully minscanf'd!\n");
+        printf("Day: %d\nMonth:%s\nYear:%d\n", day, monthname, year);
+    } else {
+        fprintf(stderr, "minscanf | Error: Arguments passed are incorrect, only successfully scanned %d args. Please try again.", amt);
+    }
 }
 
+/* minscanf:  minimal scanf with variable argument list */
 void minscanf(char* fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-    
-    for(int i = 0; i < 2; ++i) {
-        printf("n: %d\n", va_arg(ap, int));
+
+    va_list ap; //Declare argument pointer
+    char* p;
+    char* sval;
+    int ival;
+    double dval;
+
+    char options[MAXFORMAT];
+    char* optionsptr = options;
+
+    char fmtstr[MAXFORMAT];
+
+    va_start(ap, fmt); /* make ap point to 1st unnamed argument */
+    //Iterate through chars in formatted string param
+    for(p = fmt; *p; ++p) {
+        //If it's not a conv. specification, print and skip to next
+        if(*p != '%') {
+            putchar(*p);
+            continue;
+        }
+        //Iterate over for potential options
+        for(++p; !isOption(*p); ++p) {
+            //Add char to options buffer
+            *optionsptr++ = *p;
+        }
+        //Switch between the options and print!
+        switch(*p) {
+            case 'd':
+                strcpy(fmtstr, "%");
+                //Add options, if there are any
+                if(*options) {
+                    strcat(fmtstr, options);
+                }
+                strcat(fmtstr, "d");
+                ival = va_arg(ap, int);
+                printf(fmtstr, ival);                
+                break;
+            case 'f':
+                strcpy(fmtstr, "%");
+                //Add options, if there are any
+                if(*options) {
+                    strcat(fmtstr, options);
+                }
+                strcat(fmtstr, "f");
+                dval = va_arg(ap, double);
+                printf("%f", dval);
+                break;
+            case 's':
+                strcpy(fmtstr, "%");
+                //Add options, if there are any
+                if(*options) {
+                    strcat(fmtstr, options);
+                }
+                strcat(fmtstr, "s");
+                sval = va_arg(ap, char*);
+                printf("Using this format string: %s\n", fmtstr);
+                printf(fmtstr, sval);
+                break;
+            case 'c':
+                strcpy(fmtstr, "%");
+                //Add options, if there are any
+                if(*options) {
+                    strcat(fmtstr, options);
+                }
+                strcat(fmtstr, "c");
+                ival = va_arg(ap, int);
+                printf(fmtstr, ival);
+                break;
+        }
+
+        //Clear options buffer
+        *options = '\0';
+        optionsptr = options;
     }
+    va_end(ap); /* cleanup when done */
+}
+
+int isOption(int c) {
+    return c == 'd' || c == 'f' || c == 's' || c == 'c';
 }
