d289bd3c4ee6d28ccbc8c32d55445150dc195b89
Luiserebii <luis@serebii.io>
2020-01-20 21:06:20 -0500

    Made a proper Makefile for the first time here, haha. The default option, funnily enough, destroys the point of the makefile though

--- ch7/7-2/main.c
+++ ch7/7-2/main.c
@@ -6,7 +6,6 @@
 int main() {
 
     int c;
-    numprint = 0;
     while((c = getchar()) != EOF) {
         //Apply logic on whether graphic or not using iscntrl
         //Non-graphic chars are control characters
